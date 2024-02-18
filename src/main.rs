59e0cb83bcd4a269f9cae3385586e386711ae7ff
luid <eng.limaluis@gmail.com>
2024-02-18 12:12:55 -0300

    thanks navarro

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,66 @@
 use axum::{
-    routing::get,
+    http::StatusCode,
+    routing::{get,post},
     Router,
+    response::IntoResponse
 };
+
+use std::collections::HashMap;
+use time::Date;
+use uuid::Uuid;
+
+pub struct Person{
+    id: Uuid,
+    name: String,
+    nick: String,
+    birth_date: Date,
+    stack: Vec<String>,
+}
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
+
+    let mut people: HashMap<Uuid, Person> = HashMap::new();
+
+    let person: Person{
+        id: Uuid:now_v7,
+        name: String::from("Luid"),
+        nick: Sting::from("luidooo"),
+        birth_date: date!(2004 - 06 - 31)
+        stack: vec!["C".to_string(), "C++".to_string()]
+    };
+
+    //people.insert(person.id, person);
+    HashMap::insert(&mut people, person.id, person);
+
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }));
+        .route("/pessoas", get(search_peaple))
+        .route("/pessoas/:id", get(find_person))
+        .route("/pessoas", post(create_person))
+        .route("/contagem-pessoas", get(count_people));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
-    
+
     axum::serve(listener, app)
         .await
         .unwrap();
+}
+
+async fn search_peaple() -> impl IntoResponse {
+    return (StatusCode::NOT_FOUND, "Busca Pessoas")
+}
+
+async fn find_person() -> impl IntoResponse {
+    return (StatusCode::NOT_FOUND, "Find")
+}
+
+async fn create_person() -> impl IntoResponse {
+    return (StatusCode::NOT_FOUND, "Create")
+}
+
+async fn count_people() -> impl IntoResponse {
+    return (StatusCode::NOT_FOUND, "Count")
 }
